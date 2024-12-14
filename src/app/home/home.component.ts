302f6b4281f6bc4bb9b0d1526741b6baee5dca9e
sinahamidi <user21.tfa@gmail.com>
2024-12-14 17:19:40 +0330

    change some style

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
-
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { AutheticationService } from '../services/authetication.service';
+import {  MatCardModule } from '@angular/material/card';
+import { ListComponent } from '../users/list/list.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatCardModule,MatButtonModule,MatToolbarModule,RouterLink,ListComponent, RouterOutlet],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  isAdmin: boolean = false;
+  isLoggedIn: boolean = false;
+  constructor(private authService: AutheticationService, private router: Router) {}
 
+  ngOnInit(): void {
+    // this.isAdmin = this.authService.getUserRole() === 1; TODO: fix it
+    console.log(this.authService.isAuthenticated(), "wht auth?")
+    this.isLoggedIn = this.authService.isAuthenticated()
+  }
+
+  navigateToUsers() {
+    console.log("Navigating to /home/users");
+    this.router.navigate(['users'], { relativeTo: this.router.routerState.root });
+  }
+
+  navigateToAddUser() {
+    if (this.isAdmin) {
+      this.router.navigate(['/home/users/add']);
+    } else {
+      alert("You don't have permission to add users.");
+    }
+  }
+
+  logout() {
+    this.authService.logout(); 
+    this.router.navigate(['/login']);
+  }
 }
--- src/app/users/list/list.component.ts
+++ src/app/users/list/list.component.ts
@@ -3,23 +3,36 @@ import { Router, RouterModule, RouterOutlet } from '@angular/router';
 import { UserService } from '../../services/user.service';
 import { AutheticationService } from '../../services/authetication.service';
 import { MatCard, MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-list',
-  imports: [RouterOutlet, RouterModule, MatCard, MatCardModule],
+  selector: 'list',
+  imports: [RouterOutlet, RouterModule, CommonModule, FormsModule, MatCard, MatCardModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
 export class ListComponent implements OnInit {
-  users: any[] = [];
-  isAdmin = false;
+
+  users: any[] = []
+  isAdmin = false
+  editingStates: any[] = []; 
 
   constructor(
     private userService: UserService,
     private authService: AutheticationService,
     private router: Router
-  ) {}
+  ) { }
+  // ngOnInit(){
+  //   this.userService.getUsers().subscribe({
+  //     next: (d) => {
+  //       console.log(d)
+  //       return this.users = d
+  //     }
+  //   })
+  // }
 
+  
   ngOnInit(): void {
     if (!this.authService.isAuthenticated()) {
       this.router.navigate(['/login']);
@@ -55,9 +68,32 @@ export class ListComponent implements OnInit {
 
   navigateToAdd(): void {
     if (this.isAdmin) {
-      this.router.navigate(['/users/add']);
+      this.router.navigate(['/home/users/add']);
     } else {
-      this.router.navigate(['/users']);
+      this.router.navigate(['/home/users']);
     }
   }
+
+
+  startEditing(index: number, item: any): void {
+    if (!this.editingStates[index]) {
+      this.editingStates[index] = { editingIndex: index, editingRow: { ...item } };
+    }
+  }
+
+  deleteRow(index: number): void {
+    if (confirm('Are you sure you want to delete this user?')) {
+      this.users.splice(index, 1);
+      this.editingStates.splice(index, 1);  
+    }
+  }
+
+  saveEditing(index: number, item: any): void {
+    console.log('saved?', item);
+
+  }
+  cancelEditing(index: number): void {
+    this.editingStates[index] = null; 
+  }
+
 }
