38ebe630db991ea00f4a310a1221ae4fbc33e16c
sin <sinahamidi8@gmail.com>
2024-12-17 20:21:53 +0330

    fix initializing app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router, RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -6,9 +6,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { AutheticationService } from './core/auth/authetication.service';
 import { filter } from 'rxjs';
+
 @Component({
   selector: 'app-root',
-  imports: [
+imports: [
     RouterOutlet,
     RouterLink,
     MatSnackBarModule,
@@ -20,28 +21,35 @@ import { filter } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'crud';
   isLoggedIn: boolean = false;
   isLoginPage: boolean = false;
   isAdmin: boolean = false;
+  isLoading: boolean = true; 
 
-  constructor(private auth: AutheticationService, private router: Router) { }
+  constructor(private auth: AutheticationService, private router: Router) {}
+
   ngOnInit(): void {
+
     this.auth.getUserRole().subscribe({
-      next: (role) =>{
-        this.isAdmin = role === 1
+      next: (role) => {
+        this.isAdmin = role === 1;
+        this.isLoggedIn = this.auth.isAuthenticated(); 
+        this.isLoading = false; 
+      },
+      error: () => {
+        this.isLoggedIn = false;
+        this.isLoading = false;
       }
-    })
-    this.isLoggedIn = this.auth.isAuthenticated()
-    this.router.events
-      .pipe(filter((event) => event instanceof NavigationEnd))
-      .subscribe((event) => {
-        this.isLoginPage = this.router.url === '/login';
-      });
+    });
+
+    this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe(() => {
+      this.isLoginPage = this.router.url === '/login';
+    });
   }
 
-  logout() {
+  logout(): void {
     this.auth.logout();
     this.router.navigate(['/login']);
   }
